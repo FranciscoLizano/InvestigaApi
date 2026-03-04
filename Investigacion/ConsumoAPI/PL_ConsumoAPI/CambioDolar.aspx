12c907fa4a16a76844cceaa5437007e0a7fa0719
EstebanF2536 <fallasesteban07@gmail.com>
2026-03-04 16:57:59 -0600

    Esteban

---
+++ Investigacion/ConsumoAPI/PL_ConsumoAPI/CambioDolar.aspx
@@ -0,0 +1,40 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="CambioDolar.aspx.cs" Inherits="PL_ConsumoAPI.CambioDolar" Async="true" %>
+
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <link href="css/Estilos.css" rel="stylesheet" />
+    <title>Tipo de Cambio</title>
+</head>
+<body style="margin: 0; background-image: url('img/Fondo2.png'); background-size: cover; background-repeat: no-repeat;background-attachment: fixed;">
+    <form id="form1" runat="server">
+   <div style="display:flex; 
+            justify-content:center; 
+            align-items:center; 
+            gap:20px; 
+            margin-top:40px;">
+    
+    <asp:Button ID="btnConsultar" runat="server"
+        Text="Consultar tipo de cambio actual"
+        OnClick="btnConsultar_Click" 
+        CssClass="button" />
+
+    <asp:Button ID="btnHistorico" runat="server"
+        Text="Tipo de cambio ENERO 2026"
+        OnClick="btnHistorico_Click" 
+        CssClass="button" />
+
+</div>
+       <div style="margin-top:40px; text-align:center;">
+    
+    <asp:Label ID="lblResultado" runat="server"
+        CssClass="lbl" Visible="false"/>
+
+</div>
+       <div class="list">
+    <div id="divTablaHistorico" runat="server">
+    </div>
+</div>
+    </form>
+</body>
+</html>
---
+++ Investigacion/ConsumoAPI/PL_ConsumoAPI/CambioDolar.aspx.cs
@@ -0,0 +1,81 @@
+﻿using DAL_ConsumoAPI;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Services.Description;
+using System.Xml.Linq;
+
+namespace PL_ConsumoAPI
+{
+    public partial class CambioDolar : System.Web.UI.Page
+    {
+
+        protected async void btnConsultar_Click(object sender, EventArgs e)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                try
+                {
+                    cls_CambioDolar servicio = new cls_CambioDolar();
+
+                    string json = await servicio.ObtenerTipoCambioAsync();
+
+                    JObject datos = JObject.Parse(json);
+
+                    string venta = datos["venta"]["valor"].ToString();
+                    string compra = datos["compra"]["valor"].ToString();
+
+                    lblResultado.Text = "Compra: ₡" + compra +
+                                        " | Venta: ₡" + venta;
+                    lblResultado.Visible = true;
+
+                }
+                catch (Exception ex)
+                {
+                    lblResultado.Text = "Error: " + ex.Message;
+                    lblResultado.Visible = true;
+                }
+            }
+        }
+        protected async void btnHistorico_Click(object sender, EventArgs e)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                try
+                {
+                    cls_CambioDolar servicio = new cls_CambioDolar();
+                    string json = await servicio.ObtenerHistoricoCambioAsynx();
+
+                    JArray historico = JArray.Parse(json);
+
+                    string resultado = "<table class='tabla-historico'>"; resultado += "<tr>";
+                    resultado += "<td>Fecha</td>";
+                    resultado += "<td>Compra</td>";
+                    resultado += "<td>Venta</td>";
+                    resultado += "</tr>";
+
+                    foreach (var item in historico)
+                    {
+                        DateTime fecha = DateTime.Parse(item["fecha"].ToString());
+                        resultado += "<tr>";
+                        resultado += "<td>" + fecha.ToString("dd/MM/yyyy") + "</td>";
+                        resultado += "<td>₡" + item["compra"] + "</td>";
+                        resultado += "<td>₡" + item["venta"] + "</td>";
+                        resultado += "</tr>";
+                    }
+
+                    resultado += "</table>";
+
+                    divTablaHistorico.InnerHtml = resultado; 
+                }
+                catch (Exception ex)
+                {
+                    divTablaHistorico.InnerText = "Error: " + ex.Message;
+                }
+            }
+        }
+    }
+}
